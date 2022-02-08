656ee41121a2c6e1458d27df6230c1543394fe52
kitchun0402 <kitchun0402@gmail.com>
2022-02-08 17:15:31 +0800

    remove token address in the param

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,34 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Web3 from 'web3'
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Web3 from "web3";
 
-import TokenVestingApp from './views/TokenVestingApp'
+import TokenVestingApp from "./views/TokenVestingApp";
 
 const App = () => (
   <Router>
     <Switch>
-      <Route path="/:address/:token" component={ Main }/>
-      <Route component={ MissingAddress } />
+      <Route path="/:address" component={Main} />
+      <Route component={MissingAddress} />
     </Switch>
   </Router>
-)
+);
 
-const Main = function({ match }) {
-  let web3 = new Web3()
-  let { address, token } = match.params
+const Main = function ({ match }) {
+  let web3 = new Web3();
+  let { address } = match.params;
 
   // TODO validate TokenVesting address
-  return web3.utils.isAddress(address)
-    ? <TokenVestingApp address={ address } token={ token } />
-    : <MissingAddress />
-}
+  return web3.utils.isAddress(address) ? (
+    <TokenVestingApp
+      address={address}
+      /** @todo hardcoded PTP token mainnet address for now */
+      token="0x22d4002028f537599bE9f666d1c4Fa138522f9c8"
+    />
+  ) : (
+    <MissingAddress />
+  );
+};
 
-const MissingAddress = () => (
-  <p>This is not a TokenVesting address :(</p>
-)
+const MissingAddress = () => <p>This is not a TokenVesting address :(</p>;
 
-export default App
+export default App;
