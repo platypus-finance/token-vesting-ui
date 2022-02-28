d7354852e80b8aa69953bcc6bd5ee730fd6e0d3b
shinji2 <shinji@platypus.finance>
2022-02-28 21:24:33 +0800

    fix: update getAmount at

--- src/views/VestingChart.jsx
+++ src/views/VestingChart.jsx
@@ -1,109 +1,118 @@
-import React, { Component } from 'react'
-import { Line } from 'react-chartjs-2'
-import moment from 'moment'
+import React, { Component } from "react";
+import { Line } from "react-chartjs-2";
+import moment from "moment";
 
-import { displayAmount } from '../utils'
+import { displayAmount } from "../utils";
 
 class VestingChart extends Component {
   render() {
-    return <Line data={ this.chartData() } options={ this.chartOptions() } />
+    return <Line data={this.chartData()} options={this.chartOptions()} />;
   }
 
   chartData() {
     return {
       datasets: [
         this.fromBaseDataset({
-          data: this.getPoints()
+          data: this.getPoints(),
         }),
       ],
-    }
+    };
   }
 
   getPoints() {
-    const { start, cliff, end } = this.props.details
-    const now = new Date() / 1000 // normalize to seconds
+    const { start, cliff, end } = this.props.details;
+    const now = new Date() / 1000; // normalize to seconds
 
-    const points = [ this.getDataPointAt(start) ]
+    const points = [this.getDataPointAt(start)];
 
     // Add signitificant datapoints. Order matters.
     if (cliff < now) {
-      points.push(this.getDataPointAt(cliff))
+      points.push(this.getDataPointAt(cliff));
     }
 
     if (start < now && now < end) {
-      points.push(this.getDataPointAt(now))
+      points.push(this.getDataPointAt(now));
     }
 
     if (cliff > now) {
-      points.push(this.getDataPointAt(cliff))
+      points.push(this.getDataPointAt(cliff));
     }
 
-    points.push(this.getDataPointAt(end))
+    points.push(this.getDataPointAt(end));
 
-    return points
+    return points;
   }
 
   getDataPointAt(date) {
     return {
       x: this.formatDate(date),
-      y: this.getAmountAt(date)
-    }
+      y: this.getAmountAt(date),
+    };
   }
 
   formatDate(date) {
-    return moment(date * 1000).format('MM/DD/YYYY HH:mm')
+    return moment(date * 1000).format("MM/DD/YYYY HH:mm");
   }
 
   getAmountAt(date) {
-    const { total, start, end, decimals } = this.props.details
-    const slope = (date - start) / (end - start)
+    const { total, cliff, end, decimals } = this.props.details;
 
-    return displayAmount(total, decimals) * slope
+    if (date <= cliff) {
+      return 0;
+    }
+
+    const slope = (date - cliff) / (end - cliff);
+
+    return displayAmount(total, decimals) * slope;
   }
 
   chartOptions() {
     return {
       legend: { display: false },
       scales: {
-        xAxes: [ {
-          type: "time",
-          time: {
-            format: 'MM/DD/YYYY HH:mm',
-            tooltipFormat: 'll HH:mm'
+        xAxes: [
+          {
+            type: "time",
+            time: {
+              format: "MM/DD/YYYY HH:mm",
+              tooltipFormat: "ll HH:mm",
+            },
+            scaleLabel: {
+              display: true,
+              labelString: "Date",
+            },
           },
-          scaleLabel: {
-            display: true,
-            labelString: 'Date'
-          }
-        }, ],
-        yAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: this.props.details.symbol || ''
-          }
-        }]
+        ],
+        yAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: this.props.details.symbol || "",
+            },
+          },
+        ],
       },
-    }
+    };
   }
 
   fromBaseDataset(opts) {
     return {
       lineTension: 0.1,
-      backgroundColor: 'rgba(92,182,228,0.4)',
-      borderColor: 'rgba(92,182,228,1)',
-      borderJoinStyle: 'miter',
-      pointBorderColor: 'rgba(92,182,228,1)',
-      pointBackgroundColor: 'rgba(92,182,228,1)',
+      backgroundColor: "rgba(92,182,228,0.4)",
+      borderColor: "rgba(92,182,228,1)",
+      borderJoinStyle: "miter",
+      pointBorderColor: "rgba(92,182,228,1)",
+      pointBackgroundColor: "rgba(92,182,228,1)",
       pointBorderWidth: 1,
       pointHoverRadius: 5,
-      pointHoverBackgroundColor: 'rgba(92,182,228,1)',
-      pointHoverBorderColor: 'rgba(220,220,220,1)',
+      pointHoverBackgroundColor: "rgba(92,182,228,1)",
+      pointHoverBorderColor: "rgba(220,220,220,1)",
       pointHoverBorderWidth: 2,
       pointRadius: 5,
       pointHitRadius: 10,
-      ...opts
-    }
+      ...opts,
+    };
   }
 }
 
-export default VestingChart
+export default VestingChart;
